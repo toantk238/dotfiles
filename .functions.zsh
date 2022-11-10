a75477ed010d6bc0ab45e793feaae083ce96df84
rock-tran <rock.tran@manadr.com>
2022-11-10 10:08:25 +0700

    Separate functions into another file

---
+++ .functions.zsh
@@ -0,0 +1,25 @@
+nvim_edit_config ()
+{
+  CONFIG_FOLDER=$HOME/.config/nvim
+  cd $CONFIG_FOLDER
+  nvim .
+}
+
+appium_start ()
+{
+  PROJECT_DIR="/mnt/Data/Workspace/2.Personal/appium2"
+  cd $PROJECT_DIR
+  appium server -p 4723 -a 127.0.0.1 -pa /wd/hub
+}
+
+gt ()
+{
+  git status
+}
+
+gaa ()
+{
+  git add -A
+}
+
+alias gdiff="git difftool --tool=nvimdiff HEAD"
