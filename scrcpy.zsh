4448d26ea5582b90b8748a33af800e7938c2c27a
rock-tran <rock.tran@manadr.com>
2023-07-29 15:29:11 +0700

    Scrcpy : disable auto capture audio

--- scrcpy.zsh
+++ scrcpy.zsh
@@ -2,7 +2,7 @@
 
 function start_new_device() {
   device_serial="$1"
-  (tmux kill-session -t "scrcpy-$device_serial" | true ) && tmux new-session -d -s "scrcpy-$device_serial" "scrcpy -s $device_serial -m 1000"
+  (tmux kill-session -t "scrcpy-$device_serial" | true ) && tmux new-session -d -s "scrcpy-$device_serial" "scrcpy -s $device_serial -m 1000 --no-audio"
 }
 
 function scrcpy_all () {
