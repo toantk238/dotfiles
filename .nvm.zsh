b236f861ea9cf84d90ca7d9be25319009f69d5a2
rock-tran <rock.tran@manadr.com>
2023-11-05 00:35:55 +0700

    Add tmux config

--- .nvm.zsh
+++ .nvm.zsh
@@ -1,5 +1,5 @@
-source /usr/share/nvm/init-nvm.sh
+[ -f /usr/share/nvm/init-nvm.sh ] && source /usr/share/nvm/init-nvm.sh
 [ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
-source /usr/share/nvm/nvm.sh
-source /usr/share/nvm/bash_completion
-source /usr/share/nvm/install-nvm-exec
+[ -f /usr/share/nvm/nvm.sh ] && source /usr/share/nvm/nvm.sh
+[ -f /usr/share/nvm/bash_completion ] && source /usr/share/nvm/bash_completion
+[ -f /usr/share/nvm/install-nvm-exec ] && source /usr/share/nvm/install-nvm-exec
