dc01996e7f72a1a87f40b9b7169e28b5278bd7c5
rock-tran <rock.tran@manadr.com>
2023-11-29 18:11:47 +0700

    Refactor

---
+++ .android.zsh
@@ -0,0 +1,7 @@
+export ANDROID_SDK=$ANDROID_HOME
+export CLOUDSDK_PYTHON=/$HOME/.pyenv/shims/python
+
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+export PATH=$ANDROID_HOME/tools:$PATH
+export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
+export PATH=$ANDROID_HOME/emulator:$PATH
---
+++ .pyenv.zsh
@@ -0,0 +1,5 @@
+export PATH=$HOME/.pyenv/bin:$PATH
+export PYENV_ROOT=$HOME/.pyenv
+eval "$(pyenv init -)"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
