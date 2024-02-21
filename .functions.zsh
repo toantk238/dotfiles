aa955e06cd23ae707b5a128bea296c50786a7fee
rock-tran <rock.tran@manadr.com>
2024-02-21 09:48:45 +0700

    Update

--- .functions.zsh
+++ .functions.zsh
@@ -53,6 +53,8 @@ alias icat="kitty +kitten icat"
 alias docker-compose="handle_docker_compose"
 alias lc="lemonade copy"
 alias top="btop"
+alias glow="glow --pager"
+alias open="xdg-open"
 
 cdl() {
 	exe_path=$(which "$1")
