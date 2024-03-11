a7e3360c3684e3d6875b48f4e950cd4de09e99fa
rock-tran <rock.tran@manadr.com>
2024-03-12 00:17:52 +0700

    Add ranger

---
+++ .ranger.zsh
@@ -0,0 +1,10 @@
+function ranger_wrapper {
+    /usr/bin/ranger $*
+    local quit_cd_wd_file="$HOME/.cache/ranger/quit_cd_wd"
+    if [ -s "$quit_cd_wd_file" ]; then
+        cd "$(cat $quit_cd_wd_file)"
+        true > "$quit_cd_wd_file"
+    fi
+}
+
+alias rng='ranger_wrapper'
