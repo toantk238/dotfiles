a7c2a7a82af19d8940f071dedaaf665a665ba660
rock-tran <rock.tran@manadr.com>
2024-05-09 11:25:38 +0700

    Optional alias 'n' as ranger

--- .ranger.zsh
+++ .ranger.zsh
@@ -1,12 +1,14 @@
-function ranger_wrapper {
-    ranger_path=$(which ranger)
-    eval "$ranger_path $*"
-    local quit_cd_wd_file="$HOME/.cache/ranger/quit_cd_wd"
-    if [ -s "$quit_cd_wd_file" ]; then
-        cd "$(cat $quit_cd_wd_file)"
-        true > "$quit_cd_wd_file"
-    fi
-}
+if exists ranger; then
+	function ranger_wrapper {
+		ranger_path=$(which ranger)
+		eval "$ranger_path $*"
+		local quit_cd_wd_file="$HOME/.cache/ranger/quit_cd_wd"
+		if [ -s "$quit_cd_wd_file" ]; then
+			cd "$(cat $quit_cd_wd_file)"
+			true >"$quit_cd_wd_file"
+		fi
+	}
 
-alias rng='ranger_wrapper'
-alias n='ranger_wrapper'
+	alias rng='ranger_wrapper'
+	alias n='ranger_wrapper'
+fi
