da9e52662691b81d750aa03e77bfefd30053a464
rock-tran <rock.tran@manadr.com>
2024-07-09 09:57:57 +0700

    Update nvm

--- .nvm.zsh
+++ .nvm.zsh
@@ -1,5 +1,6 @@
-[ -f /usr/share/nvm/init-nvm.sh ] && source /usr/share/nvm/init-nvm.sh
+# [ -f /usr/share/nvm/init-nvm.sh ] && source /usr/share/nvm/init-nvm.sh
 [ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
+[ -f "$NVM_DIR/nvm.sh" ] && source $NVM_DIR/nvm.sh
 # [ -f /usr/share/nvm/nvm.sh ] && source /usr/share/nvm/nvm.sh
 # [ -f /usr/share/nvm/bash_completion ] && source /usr/share/nvm/bash_completion
 # [ -f /usr/share/nvm/install-nvm-exec ] && source /usr/share/nvm/install-nvm-exec
