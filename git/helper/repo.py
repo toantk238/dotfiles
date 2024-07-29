73f732b23430ecb38e0643793be2a4b3b1368e33
rock-tran <rock.tran@manadr.com>
2024-07-29 16:17:46 +0700

    Update

--- git/helper/repo.py
+++ git/helper/repo.py
@@ -26,8 +26,13 @@ class BigRepo(object):
                 logger.error(f"Repo {module} branch is not same as {main_repo_brach}. now is {module_branch}")
                 logger.error(f"You should checkout/create a branch with name {main_repo_brach}")
                 choice = input("Do you want to checkout remote branch? [y/n]: ")
-                if(choice == 'y'):
-                    module.checkout_branch(main_repo_brach)
+                if (choice == 'y'):
+                    module.pull_branch(main_repo_brach)
 
     def sub_module_repos(self) -> list[MySubmodule]:
         return list(map(lambda it: MySubmodule(it), self._repo.submodules))
+
+    def sync_branch(self):
+        main_repo_brach = self.get_main_repo_branch()
+        for module in self.sub_module_repos():
+            module.pull_branch(main_repo_brach)
