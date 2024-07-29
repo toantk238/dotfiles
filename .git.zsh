393937c9559ad332c1306b2b06716db005979237
rock-tran <rock.tran@manadr.com>
2024-07-29 16:40:31 +0700

    Update

--- .git.zsh
+++ .git.zsh
@@ -1,2 +1,6 @@
 # export GIT_PAGER="delta --show-syntax-themes --dark"
 export GIT_PAGER="delta"
+
+if exists gittool; then
+  alias gst2="gittool verify_before_push ."
+fi
--- git/helper/utils.py
+++ git/helper/utils.py
@@ -1,4 +1,5 @@
 from git import Repo
+from .log import logger
 
 
 def get_active_branch(repo: Repo):
@@ -7,8 +8,13 @@ def get_active_branch(repo: Repo):
     except:
         return None
 
+
 def is_any_changes(repo: Repo):
     unstagedFiles = [item.a_path for item in repo.index.diff(None)]
     stagedFiles = [item.a_path for item in repo.index.diff("HEAD")]
+
+    logger.debug(f"Check changes in repo {repo.working_tree_dir}")
+    logger.debug(f"Unstaged files: {unstagedFiles}")
+    logger.debug(f"stagedFiles : {stagedFiles}")
 
     return bool(unstagedFiles or stagedFiles)
