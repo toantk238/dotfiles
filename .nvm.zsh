140bc16b327cbf72bbb70e722ae24eccbab809d2
rock-tran <rock.tran@manadr.com>
2024-08-15 10:24:01 +0700

    nvm : update for macOS arm64

--- .nvm.zsh
+++ .nvm.zsh
@@ -4,3 +4,5 @@
 # [ -f /usr/share/nvm/nvm.sh ] && source /usr/share/nvm/nvm.sh
 # [ -f /usr/share/nvm/bash_completion ] && source /usr/share/nvm/bash_completion
 # [ -f /usr/share/nvm/install-nvm-exec ] && source /usr/share/nvm/install-nvm-exec
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
