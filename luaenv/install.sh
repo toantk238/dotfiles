ee037a79ef0efc8b09466bf8dcd2e931f416c4a1
rock-tran <rock.tran@manadr.com>
2024-09-21 22:00:32 +0700

    Update

--- luaenv/install.sh
+++ luaenv/install.sh
@@ -3,3 +3,10 @@
 git clone https://github.com/cehoffman/luaenv.git ~/.luaenv
 git clone https://github.com/xpol/luaenv-luarocks.git ~/.luaenv/plugins/luaenv-luarocks
 git clone https://github.com/cehoffman/lua-build.git ~/.luaenv/plugins/lua-build
+
+luaenv install 5.1.5
+luaenv global 5.1.5
+
+luaenv luarocks 2.4.3
+
+luarocks --local --lua-version=5.1 install magick
