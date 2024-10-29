2158d56ce1737f84d210756ef811c61c4467d139
rock-tran <rock.tran@manadr.com>
2024-10-29 18:09:27 +0700

    Update

--- .fzf.zsh
+++ .fzf.zsh
@@ -7,22 +7,36 @@ export FZF_DEFAULT_OPTS='--height 70% --layout=reverse --border'
 
 export FZF_ALT_C_COMMAND='fd --type directory'
 
+export RG_FZF_OPTS='--no-ignore --hidden'
+
 function rg_fzf() {
-	rg --no-ignore --hidden --color=always --line-number \
-    -i \
-    --no-heading "${*:-}" \
-    -g '!Pods/' \
-    -g '!Text.swift' \
-    -g '!*.pbxproj' \
-    -g '!DerivedData/'|
-		fzf --ansi \
-			--color "hl:-1:underline,hl+:-1:underline:reverse" \
+  rg_cmd=$(cat <<END
+rg $RG_FZF_OPTS --color=always --line-number \\
+    -i \\
+    --no-heading "${*:-}" \\
+    -g '!Pods/' \\
+    -g '!Text.swift' \\
+    -g '!*.pbxproj' \\
+    -g '!DerivedData/'
+END
+  )
+  fzf_cmd=$(cat <<END
+fzf --ansi \\
+			--color "hl:-1:underline,hl+:-1:underline:reverse" \\
 			--delimiter : \
-			--preview 'bat --color=always {1} --highlight-line {2}' \
-			--preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
-			--bind 'ctrl-w:unix-word-rubout+top,ctrl-u:unix-line-discard+top' \
-			--bind 'change:top' \
+			--preview 'bat --color=always {1} --highlight-line {2}' \\
+			--preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \\
+			--bind 'ctrl-w:unix-word-rubout+top,ctrl-u:unix-line-discard+top' \\
+			--bind 'change:top' \\
       --bind 'enter:become(nvim {1} +{2})'
-			# --bind 'enter:execute(kitty @ launch --cwd=current nvim {1} +{2})'
+END
+)
+
+  full_cmd=$(cat <<END
+$rg_cmd |
+$fzf_cmd
+END
+ )
+  eval "$full_cmd"
 }
 
