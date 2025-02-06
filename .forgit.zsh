7081c453346efc9ac2ff0798ebefe01a7e65e3ac
rock-tran <rock.tran@manadr.com>
2025-02-06 14:08:39 +0700

    Update

--- .forgit.zsh
+++ .forgit.zsh
@@ -10,8 +10,14 @@ if exists wl-copy; then
 elif exists lemonade; then
 	export FORGIT_COPY_CMD='lemonade copy --host=127.0.0.1'
 fi
+
+if exists delta; then
+  export FORGIT_PAGER="delta"
+fi
+
 export FORGIT_LOG_FZF_OPTS='
 --bind="ctrl-e:execute(echo {} |grep -Eo [a-f0-9]+ |head -1 |xargs git show |nvim -)"
 '
 export FORGIT_CHECKOUT_BRANCH_BRANCH_GIT_OPTS='--sort=-committerdate'
+
 alias gloa="glo --all"
