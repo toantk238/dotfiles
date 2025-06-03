9b2882921d5ed544ff655ea5b9c57822c8c5456d
rock-tran <rock.tran@manadr.com>
2025-06-03 16:46:33 +0700

    Auto load .nvmrc

--- .nvm.zsh
+++ .nvm.zsh
@@ -4,3 +4,28 @@
 # [ -f /usr/share/nvm/nvm.sh ] && source /usr/share/nvm/nvm.sh
 # [ -f /usr/share/nvm/bash_completion ] && source /usr/share/nvm/bash_completion
 # [ -f /usr/share/nvm/install-nvm-exec ] && source /usr/share/nvm/install-nvm-exec
+
+# place this after nvm initialization!
+autoload -U add-zsh-hook
+
+load-nvmrc() {
+  local nvmrc_path
+  nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version
+    nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
+      nvm use
+    fi
+  elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
