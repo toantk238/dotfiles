edb79d5a547541bd23be9b9646cc98883aad27f4
rock-tran <rock.tran@manadr.com>
2025-11-28 11:54:57 +0700

    Update

--- .functions.zsh
+++ .functions.zsh
@@ -314,3 +314,19 @@ osis Darwin && {
 #   alias l="ls -Al;"
 #   alias lt="ls --tree"
 # fi
+#
+
+ssh_tmux() {
+  if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then       # if this is an SSH session
+    if which tmux >/dev/null 2>&1; then                    # check if tmux is installed
+      if [[ -z "$TMUX" ]]; then                            # do not allow "tmux in tmux"
+        ID="$(tmux ls | grep -vm1 attached | cut -d: -f1)" # get the id of a deattached session
+        if [[ -z "$ID" ]]; then                            # if not available create a new one
+          tmux new-session
+        else
+          tmux attach-session -t "$ID" # if available, attach to it
+        fi
+      fi
+    fi
+  fi
+}
